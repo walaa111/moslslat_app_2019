221212c67c5a9589ff471e95e2f95d53761887b2
walaa111 <walaagamil18@gmail.com>
2020-05-03 20:51:39 +0200

    edit

--- test_cor/www/js/index.js
+++ test_cor/www/js/index.js
@@ -67,7 +67,7 @@ var app = {
 
         var inAppBrowserbRef = cordova.InAppBrowser.open('https://electrostar.ovplatform.tk', '_self', 'location=no,toolbar=no');
 
-        inAppBrowserbRef = cordova.InAppBrowser.open('http://walaa.realdeal.com.eg/moslslat_app_2019/', '_self', 'location=no,toolbar=no,zoom=no');
+        inAppBrowserbRef = cordova.InAppBrowser.open('https://walaa.realdeal.com.eg/moslslat_app_2019/', '_self', 'location=no,toolbar=no,zoom=no');
 
         //admob
      // Set AdMobAds options:
